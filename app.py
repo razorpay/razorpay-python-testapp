b013d40b2d60c9228589d2f8d6e20502fa0cf0bd
gaurav <gaurav.d@razorpay.com>
2016-07-04 16:40:44 +0530

    added flask sample app for python API

---
+++ app.py
@@ -0,0 +1,23 @@
+import razorpay
+import json
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+razorpay_client = razorpay.Client(auth=("<APP_ID>", "<APP_SECRET>"))
+
+
+@app.route('/')
+def app_create():
+    return render_template('app.html')
+
+
+@app.route('/charge', methods=['POST'])
+def app_charge():
+    amount = 5100
+    payment_id = request.form['razorpay_payment_id']
+    razorpay_client.payment.capture(payment_id, amount)
+    return json.dumps(razorpay_client.payment.fetch(payment_id))
+
+if __name__ == '__main__':
+    app.run()
